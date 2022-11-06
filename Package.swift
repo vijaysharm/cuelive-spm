545d676ca81aa4d9a3e032d63ce86d53dae99a95
Vijay Sharma <vijay.sharm@gmail.com>
2022-11-06 15:23:51 -0500

    Bump checksums

--- Package.swift
+++ Package.swift
@@ -13,17 +13,17 @@ let package = Package(
         .binaryTarget(
             name: "CUELive-framework",
             url: "https://cueaudio.jfrog.io/artifactory/spm/CUELive.3.5.9.xcframework.zip",
-            checksum: "7e55a0064a07261195480f7b139726fd37a48632707e7c6684f060b0e8586f58"
+            checksum: "7a7dfb557774f00bb91790a711adf092771a9eff8ac925af6084189bd101fb27"
         ),
         .binaryTarget(
             name: "CUEBluetooth",
             url: "https://cueaudio.jfrog.io/artifactory/spm/CUEBluetooth.0.0.12.xcframework.zip",
-            checksum: "58e921cac0a7abfa662bd53836845bfe9600541d7b3e081770d05e3c670d8342"
+            checksum: "f40017a7de5742bce69e18cf01f53800b6a220f3890f5ae1ccf3737ed53d3244"
         ),
         .binaryTarget(
             name: "engine",
             url: "https://cueaudio.jfrog.io/artifactory/spm/engine.1.46.7.xcframework.zip",
-            checksum: "52bb76b1c8a8dfe674f04a2f14a4d2f2cc576cdd83c71deda11716a8fc969076"
+            checksum: "94eccf59b5206ae6e6dd5745cfd549e0ca30f14d0829a848060304b09a0073f5"
         ),
     ]
 )
