71355bf1ca581f2e38ccbf401f5158d1188390e9
Vijay Sharma <vijay.sharm@gmail.com>
2022-11-09 15:53:08 -0500

    Bump checksums

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CUELive",
-            url: "https://s3.amazonaws.com/swift-package-manager/CUELive/3.5.9/CUELive.3.5.9.xcframework.zip",
-            checksum: "3dfef03d3cd9ad39df58e03217bd5cbbd86d7315e70652ab53d923ec80ac7183"
+            url: "https://s3.amazonaws.com/swift-package-manager/CUELive/3.5.13/CUELive.3.5.13.xcframework.zip",
+            checksum: "242ecbb4621181d75626daf309923546e20e5c57d46a1d06620e8f319613a423"
         ),
         .binaryTarget(
             name: "CUEBluetooth",
